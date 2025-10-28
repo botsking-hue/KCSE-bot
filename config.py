31c3fc19d2a024576408b1f13c87ab0d309c2dff
AHADI JOEL <joeahangila0@gmail.com>
2025-10-28 15:16:48 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Bot Configuration
+BOT_TOKEN = os.getenv('7695196775:AAE4CT_tVS7_A50BguosYSIexyA5kkoxwFA')
+MAIN_ADMIN = int(os.getenv('MAIN_ADMIN', '6501240419'))
+
+# Initial packages
+INITIAL_PACKAGES = {
+    "single": {"name": "Single Paper", "price": 2000},
+    "package_5": {"name": "5 Papers Package", "price": 8000},
+    "subscription": {"name": "All Papers Subscription", "price": 15000},
+    "school": {"name": "School Package", "price": 30000},
+    "early_bird": {"name": "Early Bird Special", "price": 1500}
+}
